3c4dd9b182b2af24487e3ec2e6c2cc6a3a19d7a3
lilyppu <lily@ppu.co.id>
2025-05-16 16:30:02 +0700

    first commit

---
+++ src/main/java/com/Screen1/Controller/package-info.java
@@ -0,0 +1,1 @@
+package com.Screen1.Controller;
---
+++ src/test/java/com/Screen1/Screen1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Screen1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Screen1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
